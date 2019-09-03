13ad97aaad61dc4d798257494c2172b4e998daee
yapo.yang <yang_yapo@126.com>
2019-09-03 17:31:15 +0800

    one more solution to  [26] 删除排序数组中的重复项

--- 26.删除排序数组中的重复项.go
+++ 26.删除排序数组中的重复项.go
@@ -3,11 +3,11 @@
  *
  * [26] 删除排序数组中的重复项
  */
-func removeDuplicates(nums []int) int {
+func removeDuplicates1(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
-	//两个指针:遍历指针和重复指针,不相等的时候覆盖重复指针并递增重复指针
+	//两个指针:遍历指针和重复指针,相邻元素不相等的时候覆盖重复指针并递增重复指针
 	di := 1
 	for i := 1; i < len(nums); i++ {
 		if nums[i] != nums[i-1] {
@@ -17,4 +17,20 @@ func removeDuplicates(nums []int) int {
 	}
 	return di
 }
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	//两个指针:遍历指针和重复指针,遍历指针与重复指针不相等的时候覆盖重复指针并递增重复指针
+	di := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[di] {
+			di++
+			nums[di] = nums[i]
+		}
+	}
+	return di + 1
+}
+
 
