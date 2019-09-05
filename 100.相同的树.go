700682c47f05dcb3ab88b14e221ec09fff0cf72c
yapo.yang <yang_yapo@126.com>
2019-09-05 13:22:20 +0800

    [100] 相同的树

---
+++ 100.相同的树.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=golang
+ *
+ * [100] 相同的树
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+//方法1: 递归; 方法二: 广度优先遍历
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if (p != nil && q == nil) || (p == nil && q != nil) {
+		return false
+	}
+	if p == nil && q == nil {
+		return true
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+
