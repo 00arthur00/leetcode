bb55b523fc79ad1e42a32360afd53f41d13c67df
yapo.yang <yang_yapo@126.com>
2020-05-08 10:03:23 +0800

    322

--- 322.零钱兑换.go
+++ 322.零钱兑换.go
@@ -3,33 +3,87 @@
  *
  * [322] 零钱兑换
  */
-func coinChange(coins []int, amount int) int {
-	if amount == 0 {
-		return 0
-	}
-	ans := make([]int, amount+1, amount+1)
-	//设置最大值为amount+1,最少硬币数必然小于amount+1,因为面额为int类型,最小值为1
-	for i := 1; i <= amount; i++ {
-		ans[i] = amount + 1
-	}
-	//自底向上,从1开始计算 f(n)
-	for i := 1; i <= amount; i++ {
-		for _, coin := range coins {
-			//币值比amount小,没有遍历必要
-			if coin <= i {
-				//取最小值并更新
-				if ans[i-coin]+1 < ans[i] {
-					ans[i] = ans[i-coin] + 1
-				}
+//自低向上dp解法
+func coinChange(coins []int, amount int) int{
+	dp:=make([]int, amount+1)
+	var i int 
+	for i=1;i<=amount;i++{
+		dp[i] = 1<<31
+		for _,c:=range coins{
+			if i-c < 0{
+				continue
+			}
+			if dp[i-c]!=-1{
+				dp[i] = min(1+dp[i-c],dp[i])
 			}
 		}
+		if dp[i]==1<<31{
+			dp[i] = -1
+		}
 	}
-	//如果为amount+1,则无解,return -1
-	if ans[len(ans)-1] > amount {
+	return dp[amount]
+
+}
+//暴力解法
+func min(a,b int)int{
+	if a<b{
+		return a
+	}
+	return b
+}
+func coinChangeRaw(coins []int, amount int) int{
+	if amount ==0{
+		return 0
+	}
+	if amount<0{
 		return -1
 	}
-	//return结果
-	return ans[len(ans)-1]
+	var res int =  1<<31
+	for _,c := range coins{
+		subProm:=coinChange(coins,amount-c)
+		if subProm ==-1{
+			continue
+		}
+		res = min(res,1+subProm)
+	}
+	if res==1<<31{
+		return -1
+	}
+	return res
+}
+//备忘录解法
+func coinChangeMemo(coins []int, amount int) int{
+	memo:=make(map[int]int)
+	
+	return dp(amount,memo,coins)
+}
+func dp(amount int,memo map[int]int, coins []int) int{
+	
+	if amount==0{
+		return 0
+	}
+	if amount<0{
+		return -1
+	}
+
+	if val,ok:=memo[amount];ok{
+		return val
+	}
+
+	res:=1<<31
+	
+	for _,c := range coins{
+		sub:=dp(amount-c,memo,coins)
+		if sub==-1{
+			continue
+		}
+		res = min(res,sub+1)
+	}
+	if res == 1<<31{
+		res = -1
+	}
+	memo[amount]=res
+	return res
 }
 
 /*
