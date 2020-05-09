c95a0d2ce49aed54590ef521bbff9e9d0cbe21a5
yapo.yang <yang_yapo@126.com>
2020-05-09 17:58:21 +0800

    617

---
+++ 617.合并二叉树.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=golang
+ *
+ * [617] 合并二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	if t1==nil&&t2==nil{
+		return nil
+	}
+
+	if t1==nil&&t2!=nil{
+		return t2
+	}
+
+	if t1!=nil&&t2==nil{
+		return t1
+	}
+
+	t1.Val += t2.Val
+	
+	t1.Left=mergeTrees(t1.Left,t2.Left)
+	t1.Right=mergeTrees(t1.Right,t2.Right)
+	return t1
+}
+// @lc code=end
+
