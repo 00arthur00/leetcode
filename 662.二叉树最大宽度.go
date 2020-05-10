d7b932edd7b120a3954a30097ab7055652e7a88e
yapo.yang <yang_yapo@126.com>
2020-05-10 22:37:25 +0800

    662

---
+++ 662.二叉树最大宽度.go
@@ -0,0 +1,120 @@
+/*
+ * @lc app=leetcode.cn id=662 lang=golang
+ *
+ * [662] 二叉树最大宽度
+ *
+ * https://leetcode-cn.com/problems/maximum-width-of-binary-tree/description/
+ *
+ * algorithms
+ * Medium (37.22%)
+ * Likes:    92
+ * Dislikes: 0
+ * Total Accepted:    6.8K
+ * Total Submissions: 18.2K
+ * Testcase Example:  '[1,3,2,5,3,null,9]'
+ *
+ * 给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。这个二叉树与满二叉树（full binary
+ * tree）结构相同，但一些节点为空。
+ * 
+ * 每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: 
+ * 
+ * ⁠          1
+ * ⁠        /   \
+ * ⁠       3     2
+ * ⁠      / \     \  
+ * ⁠     5   3     9 
+ * 
+ * 输出: 4
+ * 解释: 最大值出现在树的第 3 层，宽度为 4 (5,3,null,9)。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: 
+ * 
+ * ⁠         1
+ * ⁠        /  
+ * ⁠       3    
+ * ⁠      / \       
+ * ⁠     5   3     
+ * 
+ * 输出: 2
+ * 解释: 最大值出现在树的第 3 层，宽度为 2 (5,3)。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: 
+ * 
+ * ⁠         1
+ * ⁠        / \
+ * ⁠       3   2 
+ * ⁠      /        
+ * ⁠     5      
+ * 
+ * 输出: 2
+ * 解释: 最大值出现在树的第 2 层，宽度为 2 (3,2)。
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 
+ * 输入: 
+ * 
+ * ⁠         1
+ * ⁠        / \
+ * ⁠       3   2
+ * ⁠      /     \  
+ * ⁠     5       9 
+ * ⁠    /         \
+ * ⁠   6           7
+ * 输出: 8
+ * 解释: 最大值出现在树的第 4 层，宽度为 8 (6,null,null,null,null,null,null,7)。
+ * 
+ * 
+ * 注意: 答案在32位有符号整数的表示范围内。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+var left map[int]int
+var ans int
+func widthOfBinaryTree(root *TreeNode) int {
+	left=make(map[int]int)
+	ans = 0
+	helper(root,0,0)
+	return ans
+}
+func helper(root *TreeNode, depth,pos int){
+	if root==nil{
+		return
+	}
+	if _,ok:=left[depth];!ok{
+		left[depth] = pos
+	}
+	width :=pos - left[depth]+1 
+	
+	if width>ans{
+		ans = width
+	}
+	
+	helper(root.Left,depth+1,pos*2 +1 )
+	helper(root.Right,depth+1,pos*2 +2 )
+}
+// @lc code=end
+
