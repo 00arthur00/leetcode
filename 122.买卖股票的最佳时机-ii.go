db0cad79dc07c2d22e4c94447ce1b1de76eeab73
yapo.yang <yang_yapo@126.com>
2020-05-11 14:32:32 +0800

    122

---
+++ 122.买卖股票的最佳时机-ii.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=122 lang=golang
+ *
+ * [122] 买卖股票的最佳时机 II
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	max:=0
+	for i:=1;i<len(prices);i++{
+		if prices[i] > prices[i-1]{
+			max += prices[i]-prices[i-1]
+		}
+	}
+	return max
+}
+// @lc code=end
+
