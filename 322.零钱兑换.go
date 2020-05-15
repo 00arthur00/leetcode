2f2a358cffcbfe60c559dc4a1d970f0e7d48fbb4
yapo.yang <yang_yapo@126.com>
2020-05-15 12:29:51 +0800

    322

--- 322.零钱兑换.go
+++ 322.零钱兑换.go
@@ -4,7 +4,30 @@
  * [322] 零钱兑换
  */
 //自低向上dp解法
+func min(a,b int) int{
+	if a<b{
+		return a
+	}
+	return b
+}
 func coinChange(coins []int, amount int) int{
+	dp:=make([]int,amount+1)
+	dp[0]=0
+	for i:=1;i<=amount;i++{
+		dp[i] = amount+1
+	}
+	for _,c:=range coins{
+		for i:=c;i<=amount;i++{
+			dp[i] = min(dp[i],dp[i-c]+1)
+		}
+	}
+	if dp[amount]==amount+1{
+		return -1
+	}
+	return dp[amount]
+}
+
+func coinChange2(coins []int, amount int) int{
 	dp:=make([]int, amount+1)
 	var i int 
 	for i=1;i<=amount;i++{
